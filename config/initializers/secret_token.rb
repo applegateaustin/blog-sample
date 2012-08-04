6bb75aa33258d6e6010ee0fc48f8f0184494d2a6
Austin Applegate <theappler@gmail.com>
2012-08-03 23:16:18 -0500

    first push to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ad1138d6d4076363d867ba300a42a7a6932676e20424b41d46b59de23824ea2a4d1834c05d6b59f17f6598152fcbab0018acc1db5f87f5b687eaab3b2c88b629'
